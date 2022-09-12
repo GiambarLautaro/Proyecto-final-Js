ede16230996c2eaf3fda23ba8865a0faaf70b303
GiambarLautaro <lauti.giambartolomei@gmail.com>
2022-09-12 00:07:38 -0300

    Se agrega info al Localstorage
    se deja guardada la preferencia del usuario respecto al modo nocturno de la pagina index.html

--- main.js
+++ main.js
@@ -96,4 +96,20 @@ const btnSwitch = document.querySelector("#switch");
 btnSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   btnSwitch.classList.toggle("active");
+
+  //se guarda modo en localstorage.
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("darkMode", "true");
+  } else {
+    localStorage.setItem("darkMode", "false");
+  }
 });
+
+//obtenemos el modo actual en el cual nos encontramos
+if (localStorage.getItem("darkMode") === "true") {
+  document.body.classList.add("dark");
+  btnSwitch.classList.add("active");
+} else {
+  document.body.classList.remove("dark");
+  btnSwitch.classList.remove("active");
+}
